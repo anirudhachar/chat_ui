9dbecc2015fcfb282b54b57c344de32d75b548da
anirudhachar <anirudh@chipsy.in>
2025-12-11 13:32:29 +0530

    new1

--- app/components/ChatInterface.tsx
+++ app/components/ChatInterface.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import styles from "./ChatInterface.module.scss";
 import UserSidebar from "./UserSidebar";
 import ChatPanel from "./ChatPanel";
-import fallbackPic from "/user.png";
+
 
 
 export interface User {
@@ -255,7 +255,7 @@ if (event.data.type === "SEND_MESSAGE_TO_CHAT") {
   const chatUser: User = {
     id: user.user_id,
     name: user.firstName + " " + (user.lastName ?? ""),
-    avatar: user.profilePhoto || fallbackPic,
+    avatar: user.profilePhoto || "/user.png",
     lastMessage: "",
     lastMessageTime: "Now",
     online: true,
