af309a3ff92acd20fec6e735ebdb7b2031439480
anirudhachar <anirudh@chipsy.in>
2025-12-12 13:07:13 +0530

    hello

--- app/components/UserSidebar.tsx
+++ app/components/UserSidebar.tsx
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import { FiSearch } from 'react-icons/fi';
-import { User } from './ChatInterface';
-import styles from './UserSidebar.module.scss';
+import { FiSearch } from "react-icons/fi";
+import { User } from "./ChatInterface";
+import styles from "./UserSidebar.module.scss";
 
 interface UserSidebarProps {
   users: User[];
@@ -21,17 +21,25 @@ export default function UserSidebar({
 }: UserSidebarProps) {
   const getInitials = (name: string) => {
     return name
-      .split(' ')
-      .map(word => word[0])
-      .join('')
+      .split(" ")
+      .map((word) => word[0])
+      .join("")
       .toUpperCase()
       .slice(0, 2);
   };
 
   const getAvatarColor = (id: string) => {
     const colors = [
-      '#00a884', '#667781', '#0088cc', '#e9a944', '#9b72cb',
-      '#00897b', '#ff6b6b', '#4fb3d4', '#f06292', '#ba68c8'
+      "#00a884",
+      "#667781",
+      "#0088cc",
+      "#e9a944",
+      "#9b72cb",
+      "#00897b",
+      "#ff6b6b",
+      "#4fb3d4",
+      "#f06292",
+      "#ba68c8",
     ];
     const index = parseInt(id, 10) % colors.length;
     return colors[index];
@@ -66,27 +74,39 @@ export default function UserSidebar({
           </div>
         ) : (
           users.map((user) => (
+            console.log(user,"usersmania"),
             <div
               key={user.id}
               className={`${styles.userCard} ${
-                selectedUser?.id === user.id ? styles.active : ''
+                selectedUser?.id === user.id ? styles.active : ""
               }`}
               onClick={() => onUserSelect(user)}
             >
               <div className={styles.avatarWrapper}>
-                <div
-                  className={styles.avatar}
-                  style={{ backgroundColor: getAvatarColor(user.id) }}
-                >
-                  {getInitials(user.name)}
-                </div>
+                {user.avatar ? (
+                  <img
+                    src={user.avatar}
+                    alt={user.name}
+                    className={styles.avatarImage}
+                  />
+                ) : (
+                  <div
+                    className={styles.avatar}
+                    style={{ backgroundColor: getAvatarColor(user.id) }}
+                  >
+                    {getInitials(user.name)}
+                  </div>
+                )}
+
                 {user.online && <div className={styles.onlineIndicator} />}
               </div>
 
               <div className={styles.userInfo}>
                 <div className={styles.userHeader}>
                   <h3 className={styles.userName}>{user.name}</h3>
-                  <span className={styles.timestamp}>{user.lastMessageTime}</span>
+                  <span className={styles.timestamp}>
+                    {user.lastMessageTime}
+                  </span>
                 </div>
                 <div className={styles.lastMessageWrapper}>
                   <p className={styles.lastMessage}>{user.lastMessage}</p>
