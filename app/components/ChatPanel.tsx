8c131a4811dd12bb176a720423e474d7fcff9031
anirudhachar <anirudh@chipsy.in>
2025-12-14 12:31:54 +0530

    hello

--- app/components/ChatPanel.tsx
+++ app/components/ChatPanel.tsx
@@ -190,7 +190,7 @@ export default function ChatPanel({
         <div className={styles.userInfo}>
           <h2 className={styles.userName}>{selectedUser.name}</h2>
           <p className={styles.userStatus}>
-            {selectedUser.online ? "Online" : "Offline"}
+           "Standford University"
           </p>
         </div>
 
