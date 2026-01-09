c98e5c3de63121661525ce9a5102a30cf3982cb4
anirudhachar <anirudh@chipsy.in>
2026-01-09 20:34:00 +0530

    hello

--- app/components/ChatPanel.tsx
+++ app/components/ChatPanel.tsx
@@ -626,6 +626,26 @@ const MessageRow = ({
               isMine ? styles.mine : styles.theirs
             }`}
           >
+            <svg
+              viewBox="0 0 8 13"
+              width="8"
+              height="13"
+              preserveAspectRatio="xMidYMid meet"
+              className={`${styles.tail} ${
+                isMine ? styles.tailMine : styles.tailTheirs
+              }`}
+            >
+              <path
+                opacity="0.13"
+                d="M5.188,1H0v11.193l6.467-8.625
+         C7.526,2.156,6.958,1,5.188,1z"
+              />
+              <path
+                fill="currentColor"
+                d="M5.188,0H0v11.193l6.467-8.625
+         C7.526,1.156,6.958,0,5.188,0z"
+              />
+            </svg>
             <div
               className={`${styles.senderNameLabel} ${
                 isMine ? styles.textRight : styles.textLeft
